0fcc282b6c55040295753529a9147c8262872bdf
AC <a@a.a>
2018-09-22 13:03:27 -0700

    started adding controller logic and add interface

--- Cards/src/controllers/LoginController.java
+++ Cards/src/controllers/LoginController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class LoginController {
 	
 	
--- Cards/src/services/interfaces/ILoginBusinessService.java
+++ Cards/src/services/interfaces/ILoginBusinessService.java
@@ -1,5 +1,7 @@
 package services.interfaces;
 
 public interface ILoginBusinessService {
-
+	
+	
+	
 }
