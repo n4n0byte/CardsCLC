ddff34f7093f925ae16d4c5eba3ee74d451394ed
AC <a@a.a>
2018-10-15 22:23:01 -0700

    updated credentials services

--- Cards/src/com/services/implementations/MockCredentialsDataService.java
+++ Cards/src/com/services/implementations/MockCredentialsDataService.java
@@ -1,8 +1,24 @@
 package com.services.implementations;
 
+import com.models.User;
 import com.services.interfaces.ICredentialsDataService;
 
 public class MockCredentialsDataService implements ICredentialsDataService{
+
+	@Override
+	public boolean isRegistered(User user) {
+		return true;
+	}
+
+	@Override
+	public boolean tryRegisterUser(User user) {
+		return true;
+	}
+
+	@Override
+	public boolean isValidCredentials(User user) {
+		return true;
+	}
 	
 	
 	
